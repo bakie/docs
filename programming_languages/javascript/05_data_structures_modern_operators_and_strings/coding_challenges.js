d72d6546f0c69a6e15d18a8a1a21c54fd270a591
Bakie <bakelants.developer@gmail.com>
2023-02-14 11:01:36 +0100

    javascrip coding challenge 2

--- programming_languages/javascript/05_data_structures_modern_operators_and_strings/coding_challenges.js
+++ programming_languages/javascript/05_data_structures_modern_operators_and_strings/coding_challenges.js
@@ -85,3 +85,44 @@ console.log(...game.scored)
 // without using an if/else statement or the ternary operator.
 team1 < team2 && console.log('Team 1');
 team1 > team2 && console.log('Team 2');
+
+// Coding Challenge #2
+console.log('CODING CHALLENGE #2');
+// 1. Loop over the game.scored array and print each player name to the console,
+// along with the goal number (Example: "Goal 1: Lewandowski")
+for (const [index, player] of game.scored.entries()) {
+    console.log(`Goal ${index + 1}: ${player}`);
+}
+
+// 2. Use a loop to calculate the average odd and log it to the console (We already
+// studied how to calculate averages, you can go check if you don't remember)
+let average = 0;
+for (const odd of Object.values(game.odds)) {
+    average += odd;
+}
+console.log(`The average odd is: ${average / Object.values(game.odds).length}`);
+
+// 3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+//        Odd of victory Bayern Munich: 1.33
+//        Odd of draw: 3.25
+//        Odd of victory Borrussia Dortmund: 6.5
+//    Get the team names directly from the game object, don't hardcode them (except for "draw").
+//    Hint: Note how the odds and the game objects have the same property names.
+for (const [key, value] of Object.entries(game.odds)) {
+   console.log(`Odd of ${key === 'x' ? 'draw' : 'victory ' + game[key]}: ${value}`);
+}
+
+// 4. Bonus: Create an object called 'scorers' which contains the names of the
+//    players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+//      {
+//        Gnarby: 1,
+//        Hummels: 1,
+//        Lewandowski: 2
+//      }
+//
+//  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+const scorers = {};
+for (const player of game.scored) {
+    scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+}
+console.log(scorers);
