d71d226a6c3b295d67b580d19cc6a3f2e48ff8aa
Bakie <bakelants.developer@gmail.com>
2023-03-24 10:56:55 +0100

    js coding challenge 3

--- programming_languages/javascript/10_object_oriented_programming/coding_challenges.js
+++ programming_languages/javascript/10_object_oriented_programming/coding_challenges.js
@@ -60,3 +60,26 @@ ford.brake();
 console.log(`${ford.make} is going at ${ford.speedUS} mi/h`);
 ford.speedUS = 100;
 console.log(`${ford.make} is going at ${ford.speedUS} mi/h`);
+
+// Coding Challenge #3
+console.log('CODING CHALLENGE #3');
+const EV = function(make, speed, charge) {
+    Car.call(this, make, speed);
+    this.charge = charge;
+}
+
+EV.prototype = Object.create(Car.prototype);
+EV.prototype.chargeBattery = function(chargeTo) {
+    this.charge = chargeTo;
+}
+EV.prototype.accelerate = function() {
+    this.speed += 20;
+    this.charge--;
+    console.log(`${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}%`);
+};
+
+const tesla = new EV('Tesla', 120, 23);
+tesla.accelerate();
+tesla.brake();
+tesla.chargeBattery(90);
+tesla.accelerate();
