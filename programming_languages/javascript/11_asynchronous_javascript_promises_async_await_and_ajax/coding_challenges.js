c88093501a6e654c3eaddf31e559ca5b5b9f2a69
Bakie <bakelants.developer@gmail.com>
2023-03-29 11:36:48 +0200

    js coding challenge 1

--- programming_languages/javascript/11_asynchronous_javascript_promises_async_await_and_ajax/coding_challenges.js
+++ programming_languages/javascript/11_asynchronous_javascript_promises_async_await_and_ajax/coding_challenges.js
@@ -2,3 +2,27 @@
 
 // Coding Challenge #1
 console.log('CODING CHALLENGE #1');
+const whereAmi = function(lat, lng) {
+    fetch(`https://geocode.xyz/${lat},${lng}?geoit=json&auth=API_KEY_REQUIRED`)
+    .then(response => {
+        if (!response.ok) throw new Error(`Something went wrong ${response.status}`);
+
+        return response.json();
+    })
+    .then(result => {
+        console.log(`You are in ${result.city}, ${result.country}`);
+
+        return fetch(`https://restcountries.com/v3.1/name/${result.country}`);
+    })
+    .then(response => {
+        if (!response.ok) throw new Error(`Something went wrong ${response.status}`);
+
+        return response.json();
+    })
+    .then(result => console.log(result[0]))
+    .catch(err => console.error(err.message));
+}
+
+//whereAmi(52.508, 13.381);
+//setTimeout(() => whereAmi(19.037, 72.873), 1000);
+//setTimeout(() => whereAmi(-33.933, 18.474), 2000);
