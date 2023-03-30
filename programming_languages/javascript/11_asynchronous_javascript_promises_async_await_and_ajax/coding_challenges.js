63a3f6e1c1ac7a4437c96207f54ffd1fafe4c78a
Bakie <bakelants.developer@gmail.com>
2023-03-30 10:44:31 +0200

    js coding challenge 2

--- programming_languages/javascript/11_asynchronous_javascript_promises_async_await_and_ajax/coding_challenges.js
+++ programming_languages/javascript/11_asynchronous_javascript_promises_async_await_and_ajax/coding_challenges.js
@@ -26,3 +26,51 @@ const whereAmi = function(lat, lng) {
 //whereAmi(52.508, 13.381);
 //setTimeout(() => whereAmi(19.037, 72.873), 1000);
 //setTimeout(() => whereAmi(-33.933, 18.474), 2000);
+
+// Coding Challenge #2
+console.log('CODING CHALLENGE #2');
+
+const imgContainer = document.querySelector('.images');
+let currentImg;
+
+const createImage = function(imgPath) {
+    return new Promise(function(resolve, reject) {
+        const img = document.createElement('img');
+        img.src = imgPath;
+
+        img.addEventListener('load', function() {
+            imgContainer.append(img);
+            resolve(img);
+        });
+
+        img.addEventListener('error', function() {
+            reject(new Error('Image not found'));
+        });
+    });
+};
+
+const wait = function(seconds) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, seconds * 1000);
+    });
+};
+
+createImage('coding_images/black.png')
+    .then(img => {
+        console.log('First image is loaded');
+        currentImg = img;
+        return wait(2);
+    })
+    .then(() => {
+        currentImg.style.display = 'none';
+        return createImage('coding_images/white.png');
+    })
+    .then(img => {
+        console.log('Second image is loaded');
+        currentImg = img;
+        return wait(2);
+    })
+    .then(() => {
+        currentImg.style.display = 'none';
+    })
+    .catch(err => console.error(err));
